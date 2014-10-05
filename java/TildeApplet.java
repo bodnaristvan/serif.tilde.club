00c4026ac41368e454b2f0fdd362891f8e6a45a3
Istvan Bodnar <istvan@gawker.com>
2014-10-05 22:44:51 +0200

    Added java applet!

---
+++ java/TildeApplet.java
@@ -0,0 +1,8 @@
+import java.applet.*;
+import java.awt.*;
+
+public class TildeApplet extends Applet {
+	public void paint (Graphics g) {
+		g.drawString("Hello my dearest readers!", 25, 50);
+	}
+}
